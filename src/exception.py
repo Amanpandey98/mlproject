cee4ea0453e4c4a420dbb86a76ec08856e335819
Aman pandey <amanpandey1145@gmail.com>
2024-09-23 12:06:15 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_details:sys):
@@ -18,6 +18,11 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+
+
+
     
 
         
